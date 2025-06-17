174c1a9d335b4cf69e34df5aff6e9805c55e1fd3
Ajeet Singh <ajeet.s@skillbrew.com>
2025-06-17 22:25:15 +0530

    Update

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,6 @@
-import "./global.css";
-import "./all.min.css";
 import "./bootstrap.min.css";
+import "./fontawesome.min.css";
+import "./all.min.css";
+import "./global.css";
 import "./style.css";
 import "./responsive.css";
