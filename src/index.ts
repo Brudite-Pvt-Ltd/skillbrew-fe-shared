bdcf1df4338bef0a71d6752153cbd5b4a1ff5923
Ajeet Singh <ajeet.s@skillbrew.com>
2025-06-19 11:22:53 +0530

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './utils/Helper';
-export * from './utils/Icons';
+export { default as Icons } from './utils/Icons';
 export * from './utils/Json';
 export * from './utils/playgroundHelper';
 export * from './utils/Types';
